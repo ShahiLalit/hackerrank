5ed602b0f66a4433c35fa2dd94e3dc674d425412
Lalit Singh Shahi <shahilalit002@gmail.com>
2021-05-30 13:16:52 +0530

    jumping on clouds with for loop

--- warmup/jumping_on_clouds.js
+++ warmup/jumping_on_clouds.js
@@ -16,4 +16,19 @@ function jumpingOnClouds(c) {
   return jumps;
 }
 
-jumpingOnClouds([0, 0, 1, 0, 1, 0, 0, 1, 0]);
+jumpingOnClouds([0, 0, 1, 0, 0, 0, 0, 1, 0, 0]);
+
+// Method 2 - Using For Loop
+
+function jumpingOnClouds2(c) {
+  // Write your code here
+  let jumps = 0;
+  for (let index = 0; index < c.length - 1; index++, jumps++) {
+    if (c[index + 2] === 0) {
+      index++;
+    }
+  }
+  return jumps;
+}
+
+jumpingOnClouds2([0, 0, 1, 0, 0, 1, 0]);
