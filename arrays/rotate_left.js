ce1c7463295e80e9cf0ca4b79568920741353523
Lalit Singh Shahi <shahilalit002@gmail.com>
2021-05-31 22:54:08 +0530

    method 2 for rotate left array

--- arrays/rotate_left.js
+++ arrays/rotate_left.js
@@ -8,5 +8,19 @@ function rotLeft(a, d) {
   console.log(newArr);
   return newArr;
 }
-
 rotLeft([1, 2, 3, 4, 5], 4);
+
+// Method 2
+function rotLeft2(a, d) {
+  // Write your code here
+  let newArr = [];
+  let arrLen = a.length;
+  for (let i = 0; i < arrLen; i++) {
+    const newIndex = (i + arrLen - d) % arrLen;
+    console.log(newIndex);
+    newArr[newIndex] = a[i];
+  }
+  console.log(newArr);
+  return newArr;
+}
+rotLeft2([1, 2, 3, 4, 5], 4);
